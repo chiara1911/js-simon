07cee0e0c710582d7db5e748944af4a25749b655
chiara1911 <chiaracesari89@gmail.com>
2023-10-22 19:16:48 +0200

    add third array with equal element and add compare

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 // 1. Visualizzare in pagina 5 numeri casuali.Da lì parte un timer di 30 secondi.
 
+const simon = document.getElementById('simon'); //costante h3 con dentro i numeri random
+let quantitàNumRandom = 2;
+let number = []; //array numeri random
+let numUser = []; // array numeri utente
+let arrNum = [];
+let equalNum = [];
+let send = document.querySelector('button'); //bottone inizia memory
+const sendResult = document.getElementById('sendResult'); //bottone Indovina
+let utente = document.querySelector('h1');
 // creo un array per mettere i 5 numeri casuali
-
-
-// ;
-const simon = document.getElementById('simon');  
-let number = numRandom(5);
-
-const send = document.querySelector('button');
-console.log(simon);
-// funzione per generare 5 numeri random
-
-function numRandom(x, z) {
+function numRandom(x) {
     y = [];
     let i = 0;
     while (i < x) {
@@ -19,20 +18,61 @@ function numRandom(x, z) {
         y.push(z)
         i++
     }
-    console.log(y);
     return y;
 }
-send.addEventListener('click', function(){     
-    simon.innerHTML = number;       
-    let memory = setTimeout(nascosto, 3000);
-function nascosto (){
-   simon.remove(number);
-}       
+// funzione per far sparire i numeri dopo 3 secondi, timer parte dal click
+send.addEventListener('click', function () {
+    number = numRandom(quantitàNumRandom); // quantità numeri random 
+    console.log('numeri random' + number);
+    arrNum = number;
+    simon.innerHTML = number;
+    send = setTimeout(nascosto, 3000);
+    function nascosto() {
+        simon.remove(number);
+    }
 });
 
 
+//ogni volta che clicco il bottone, mette il numero inserito in Array
+//guardare se corrispondono i numeri da un array all'altro
+sendResult.addEventListener('click', function () {
+    result = parseInt(document.getElementById('result').value);
+    // mettere i numeri da input in array nuovo        
+    numUser.push(result);
+    // se gli elementi del primo array sono uguali al secondo array(tramite array3)    
+    if (numUser.length === quantitàNumRandom) {
+        equalNum = equalArr(arrNum, numUser);
+        console.log('equalNum' + equalNum);
+        if (equalNum.length === quantitàNumRandom) {
+            console.log('hai vinto');
+            text = 'Hai Indovinato tutti i numeri';
+        }
+        else{
+            text = 'Non hai indovinato tutti i numeri, quindi hai perso perchè hai indovinato solo ' + equalNum;
+        }
+    }        
+    else {
+        console.log('continua...');
+        text = 'continua...';
+    }
+    utente.innerHTML = text;
+});
 
-// funzione per stampare in pagina i numeri per 30 secondi
+function equalArr(arr1, arr2) {
+
+    let arr3 = [];
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr1.includes(arr2[i])) {
+            arr3.push(arr2[i]);
+        }
+    }
+    return arr3;
+}
+
+
+
+
+
 
 
 
